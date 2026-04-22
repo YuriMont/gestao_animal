1301d364a0ebca3a160f73dc1d3a3e7c2e048018
Yuri Monteiro <yuri.mont1043@gmail.com>
2026-04-22 20:12:31 -0300

    order inputs

--- apps/web/src/routes/production.tsx
+++ apps/web/src/routes/production.tsx
@@ -1,39 +1,39 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { AppLayout } from '@/components/layout/app-layout'
-import { PageHeader } from '@/components/layout/page-header'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { WeightForm } from '@/components/production/weight-form'
-import { MilkForm } from '@/components/production/milk-form'
+import { AppLayout } from "@/components/layout/app-layout";
+import { PageHeader } from "@/components/layout/page-header";
+import { MilkForm } from "@/components/production/milk-form";
+import { WeightForm } from "@/components/production/weight-form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/production')({
-  component: ProductionPage,
-})
+export const Route = createFileRoute("/production")({
+	component: ProductionPage,
+});
 
 function ProductionPage() {
-  return (
-    <AppLayout>
-      <div className="flex flex-col">
-        <PageHeader
-          title="Produção"
-          description="Registre pesos e produção de leite do rebanho"
-        />
-        <div className="p-6 mx-auto w-full max-w-lg">
-          <Tabs defaultValue="weight" className="w-full">
-            <TabsList className="w-full grid grid-cols-2">
-              <TabsTrigger value="weight">Pesagem</TabsTrigger>
-              <TabsTrigger value="milk">Produção de Leite</TabsTrigger>
-            </TabsList>
+	return (
+		<AppLayout>
+			<div className="flex flex-col">
+				<PageHeader
+					title="Produção"
+					description="Registre pesos e produção de leite do rebanho"
+				/>
+				<div className="p-6 mx-auto w-full max-w-lg">
+					<Tabs defaultValue="weight" className="w-full">
+						<TabsList className="w-full grid grid-cols-2">
+							<TabsTrigger value="weight">Pesagem</TabsTrigger>
+							<TabsTrigger value="milk">Produção de Leite</TabsTrigger>
+						</TabsList>
 
-            <TabsContent value="weight" className="mt-4">
-              <WeightForm />
-            </TabsContent>
+						<TabsContent value="weight" className="mt-4">
+							<WeightForm />
+						</TabsContent>
 
-            <TabsContent value="milk" className="mt-4">
-              <MilkForm />
-            </TabsContent>
-          </Tabs>
-        </div>
-      </div>
-    </AppLayout>
-  )
+						<TabsContent value="milk" className="mt-4">
+							<MilkForm />
+						</TabsContent>
+					</Tabs>
+				</div>
+			</div>
+		</AppLayout>
+	);
 }
